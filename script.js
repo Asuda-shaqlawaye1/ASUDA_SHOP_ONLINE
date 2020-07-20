2326a0ba8c44ef07a32bfb2aa594adfff656d956
AsudaShaqlawaye <asuda.xoshnaw12@gmail.com>
2020-07-20 13:34:28 -0700

    ASUDA_SHOP_ONLINE

---
+++ script.js
@@ -0,0 +1,13 @@
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 6,
+  spaceBetween: 0,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+
+//siper slider 3d
+
